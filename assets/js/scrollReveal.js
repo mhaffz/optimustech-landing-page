0636fc7d1ba9a1c5981a5643eda6c24fb3d6d0db
Mateus Haffermann Farias <mateushaffmain@gmail.com>
2022-12-09 01:52:38 -0400

    adding footer

--- assets/js/scrollReveal.js
+++ assets/js/scrollReveal.js
@@ -1,6 +1,6 @@
 ScrollReveal({ reset: true });
 
-ScrollReveal().reveal(".about, .metrics, .jobs__presentation, .jobs__description", {
+ScrollReveal().reveal(".about, .metrics, .jobs__presentation, .jobs__description, .collaborators, .footer", {
     distance: '-50px',
     duration: 1500
 });
